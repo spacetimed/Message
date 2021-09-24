c79adb938827e3e25e2102a4e0708fefbbf38865
FFFFFF <s127813127@gmail.com>
2021-09-24 03:08:55 -0700

    Client class, handle disconnect through Server class

--- server/Message/lib.py
+++ server/Message/lib.py
@@ -2,36 +2,59 @@ import asyncio
 import websockets
 from colorama import Fore, Back, Style
 
+from typing import List
+from typing import Type
+from typing import Any
+
 class Server:
+    
     def __init__(self, host: str, port: int) -> None:
         self.host: str = host
         self.port: int = port
         self.start: websockets.WebSocketServer = websockets.serve(self.handleNewClient, self.host, self.port)
-        self.clients = []
-        self.log = Logger(__name__ + '/' + __class__.__name__)
+        self.clients: List[Type[Client]] = []
+        self.log: Type[Logger] = Logger(__name__ + '/' + __class__.__name__)
         if self.start is not None:
             self.log('Started! Listening...')
 
-    async def handleNewClient(self, websocket, path):
-        print('New Client=>', websocket, path)
+    async def handleNewClient(self, websocket: Any, path: Any) -> None:
+        clientId: int = len(self.clients)
+        self.clients.append(Client(clientId, websocket, path, self))
+        await self.clients[clientId].serve()
+
+    def handleDisconnect(self, clientId: int) -> None:
+        if clientId <= len(self.clients):
+            self.clients.pop(clientId)
+        else:
+            self.log(f'Disconnect error (Client #{str(clientId)})')
+        return None
+
+
+class Client():
+
+    def __init__(self, clientId: int, websocket: Any, path: Any, MasterServer: Type[Server]) -> None:
+        self.clientId: int = clientId
+        self.websocket: Any = websocket
+        self.path: Any = path
+        self.MasterServer: Type[Server] = MasterServer
+        print('New Client! #', clientId)
+
+    async def serve(self) -> None:
         try:
             while True:
-                message = await websocket.recv()
+                message = await self.websocket.recv()
                 print('Recv=>', message)
         except websockets.ConnectionClosed:
-            print('DC')
-
-
-class Client:
-    def __init__(self, websocket, path):
-        print('New Client!')
+            self.MasterServer.handleDisconnect(self.clientId)
+            print('Dc')
 
 
 class Logger:
-    def __init__(self, name: str):
+
+    def __init__(self, name: str) -> None:
         self.name: str = name
         if 'Server' in name:
-            logo = """
+            logo: str = """
       `:+oo+/-`     
    `odMMMMMMMMNy:   
   -mMMMMMMMMMMMMMs          Welcome to Message!
@@ -42,5 +65,6 @@ class Logger:
    -dMmy:...`       
  `oM:"""                   
             print(logo, end='\n\n')
-    def __call__(self, message: str):
+
+    def __call__(self, message: str) -> None:
         print(f'{Fore.RED}{self.name.ljust(20)}{Style.RESET_ALL}{message}')
