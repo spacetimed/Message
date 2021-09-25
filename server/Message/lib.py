366d19927e2656e5f6794f47e07381701f4fd83e
FFFFFF <s127813127@gmail.com>
2021-09-24 18:07:34 -0700

    Fixed client disconnect bug

--- server/Message/lib.py
+++ server/Message/lib.py
@@ -1,10 +1,21 @@
 import asyncio
 import websockets
+
 from colorama import Fore, Back, Style
+from time import time as getTimestamp
+from hashlib import sha256
 
 from typing import List
 from typing import Type
 from typing import Any
+
+"""
+    To Do:
+    - Client logger subclass
+    - Send connection successful message handshake, set local variables on client
+    - Show login prompt
+    - Make database tables (`users`, `messages[?]`)
+"""
 
 class Server:
     
@@ -13,7 +24,7 @@ class Server:
         self.port: int = port
         self.start: websockets.WebSocketServer = websockets.serve(self.handleNewClient, self.host, self.port)
         self.clients: List[Type[Client]] = []
-        self.log: Type[Logger] = Logger(__name__ + '/' + __class__.__name__)
+        self.log: Type[Logger] = Logger(f'{__name__}/{__class__.__name__}')
         if self.start is not None:
             self.log('Started! Listening...')
 
@@ -22,9 +33,17 @@ class Server:
         self.clients.append(Client(clientId, websocket, path, self))
         await self.clients[clientId].serve()
 
-    def handleDisconnect(self, clientId: int) -> None:
-        if clientId <= len(self.clients):
-            self.clients.pop(clientId)
+        #if(clientId == 0):
+        #    await self.showClientList()
+
+    """async def showClientList(self):
+        while True:
+            print(self.clients)
+            await asyncio.sleep(1) """
+
+    def handleDisconnect(self, clientObject, clientId) -> None:
+        if clientObject in self.clients:
+            self.clients.remove(clientObject)
         else:
             self.log(f'Disconnect error (Client #{str(clientId)})')
         return None
@@ -37,16 +56,22 @@ class Client():
         self.websocket: Any = websocket
         self.path: Any = path
         self.MasterServer: Type[Server] = MasterServer
-        print('New Client! #', clientId)
+        self.clientHash: str = self.makeClientHash()
+        self.log: Type[Logger] = Logger(f'{__name__}/{__class__.__name__}::{self.clientId}')
+        self.log('New connection!')
 
     async def serve(self) -> None:
         try:
             while True:
                 message = await self.websocket.recv()
-                print('Recv=>', message)
+                print(self.clientHash, 'Recv=>', message)
         except websockets.ConnectionClosed:
-            self.MasterServer.handleDisconnect(self.clientId)
-            print('Dc')
+            self.log('Disconnect')
+            self.MasterServer.handleDisconnect(self, self.clientId)
+
+    def makeClientHash(self) -> str:
+        toEncode = (str(getTimestamp()) + str(self.clientId)).encode('utf-8')
+        return sha256(toEncode).hexdigest()[:8]
 
 
 class Logger:
@@ -67,4 +92,4 @@ class Logger:
             print(logo, end='\n\n')
 
     def __call__(self, message: str) -> None:
-        print(f'{Fore.RED}{self.name.ljust(20)}{Style.RESET_ALL}{message}')
+        print(f'{Fore.RED}{self.name.ljust(26)}{Style.RESET_ALL}{message}')
